703a870eaab5d2b7a28992a38b3c8d3c4ab45e4b
RithyBondeth <rithybondeth999@gmail.com>
2025-01-01 22:51:42 +0700

    profile page in progress

--- src/pages/sub/profile/index.vue
+++ src/pages/sub/profile/index.vue
@@ -1,7 +1,17 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div>
-    <h1>Profile Page</h1>
+  <div class="h-screen w-full p-5 flex flex-col items-start gap-5">
+    <!-- Header Label -->
+    <p class="text-2xl font-bold">Profile</p>
+
+    <!-- Profile Section -->
+    <div class="size-full flex justify-between items-stretch gap-5">
+      <!-- Left Side Section -->
+      <div class="w-1/3 bg-blue-400"></div>
+
+      <!-- Right Side Section -->
+      <div class="w-2/3 bg-red-400"></div>
+    </div>
   </div>
 </template>
